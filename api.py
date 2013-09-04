7ea37a4f77c8c4793062c5027bae9e9799c82ee2
Erik Romijn <erik@erik.io>
2013-09-04 22:27:36 +0200

    minor code cleanup

--- api.py
+++ api.py
@@ -7,14 +7,14 @@ from flask import Flask, Response
 import config
 
 app = Flask(__name__)
-mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+mc = memcache.Client(config.MEMCACHE_SERVERS, debug=0)
 
 @app.route("/actueel/")
 def actueel():
     data = mc.get(config.MEMCACHE_KEY)
     if not data:
         headers = {'User-Agent': 'weerapi -- https://github.com/erikr/weerapi'}
-        data = requests.get('http://m.knmi.nl/index.php?i=Actueel&s=tabel_10min_data', headers=headers).text
+        data = requests.get(config.URL, headers=headers).text
         mc.set(config.MEMCACHE_KEY, data, time=config.MEMCACHE_EXPIRY)
 
     results = {}
@@ -26,7 +26,6 @@ def actueel():
             continue
 
         fields = [field.text.replace('&nbsp;', '').strip() for field in fields]
-
         (name, weather_type, temperature, humidity, wind_direction, wind_speed, visibility, pressure) = fields
 
         if name and name in config.LOCATION_MAPPING:
@@ -50,10 +49,11 @@ def actueel():
             'pressure': pressure,
         }
 
+    timestamp = soup.find('div', {'class': 'alineakop'}).text.replace('Waarnemingen ', '')
     response = {
         'actueel': results,
         'source': 'KNMI',
-        'timestamp': soup.find('div', {'class': 'alineakop'}).text.replace('Waarnemingen ', '')
+        'timestamp': timestamp
     }
     return Response(response=json.dumps(response, indent=4),
                     status=200,
@@ -64,8 +64,8 @@ if not app.debug:
     import logging
     from logging.handlers import SMTPHandler
     mail_handler = SMTPHandler('127.0.0.1',
-                               'eromijn@solidlinks.nl',
-                               'eromijn@solidlinks.nl', 'Weer error')
+                               'wegh1@solidlinks.nl',
+                               'wegh1@solidlinks.nl', 'Weer error')
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
